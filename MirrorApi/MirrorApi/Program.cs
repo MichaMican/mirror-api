2a16820ab0baec5ef174cb33b51dc971aca24910
Alexander Mnich <56564725+a-mnich@users.noreply.github.com>
2023-12-06 18:11:20 +0100

    split body if content is too long for one property

--- MirrorApi/MirrorApi/Program.cs
+++ MirrorApi/MirrorApi/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.ApplicationInsights.DataContracts;
-using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,11 +21,19 @@ app.MapControllers();
 
 app.Run(async (context) =>
 {
+    int CHUNK_SIZE = 8192;
+
     context.Response.StatusCode = StatusCodes.Status200OK;
     context.Response.Headers["Content-Type"] = context.Request.Headers["Content-Type"];
-    var rawRequestBody = await (new StreamReader(context.Request.Body).ReadToEndAsync());
+    var rawRequestBody = await new StreamReader(context.Request.Body).ReadToEndAsync();
     var requestTelemetry = context.Features.Get<RequestTelemetry>();
-    requestTelemetry?.Properties.Add("RequestBody", rawRequestBody);
+    var chunks = Enumerable.Range(0, (rawRequestBody.Length / CHUNK_SIZE) + 1)
+        .Select(i => rawRequestBody.Substring(i * CHUNK_SIZE, Math.Min(CHUNK_SIZE, rawRequestBody.Length - i * CHUNK_SIZE)))
+        .ToList();
+    for (int i = 0; i < chunks.Count; i++)
+    {
+        requestTelemetry?.Properties.Add($"RequestBody_{i + 1}", chunks[i]);
+    }
     requestTelemetry?.Properties.Add("RequestContentTypeHeader", context.Request.Headers["Content-Type"]);
     requestTelemetry?.Properties.Add("RequestContentLengthHeader", context.Request.Headers["Content-Length"]);
     await context.Response.WriteAsync(rawRequestBody);
